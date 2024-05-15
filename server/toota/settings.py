5dfbec9f2c26bf772ef0054394c687de205b2464
Marlvin Kwenda <vinnywalker96@icloud.com>
2024-05-15 22:09:47 +0200

    fix staging

--- server/toota/settings.py
+++ server/toota/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-aov%#o5ft#6c85j_3&vghmb^m(vri==)4r0sfxlk6sz%r-mqrt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
