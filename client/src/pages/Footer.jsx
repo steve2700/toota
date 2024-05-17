c1b8c62977137d732aa388cec38f76c8c5ba2796
steve2700 <stevnyaru@github.com>
2024-05-17 15:51:43 +0200

    fixed the ui on the layout page now smooth and clean

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -5,22 +5,22 @@ import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaEnvelope } from 'reac
 
 const Footer = () => {
     return (
-        <footer className="bg-white text-gray-900 py-8">
+        <footer className="bg-white text-[#404042] py-8">
             <div className="container mx-auto px-4">
                 <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
                     {/* Social Media Links */}
                     <div className="col-span-2 md:col-span-3 flex items-center justify-center md:justify-start">
                         <a href="#" className="mr-4">
-                            <FaFacebook className="text-2xl hover:text-blue-500" />
+                            <FaFacebook className="text-2xl hover:text-[#f89f1b]" />
                         </a>
                         <a href="#" className="mr-4">
-                            <FaTwitter className="text-2xl hover:text-blue-500" />
+                            <FaTwitter className="text-2xl hover:text-[#f89f1b]" />
                         </a>
                         <a href="#" className="mr-4">
-                            <FaInstagram className="text-2xl hover:text-blue-500" />
+                            <FaInstagram className="text-2xl hover:text-[#f89f1b]" />
                         </a>
                         <a href="#" className="mr-4">
-                            <FaLinkedin className="text-2xl hover:text-blue-500" />
+                            <FaLinkedin className="text-2xl hover:text-[#f89f1b]" />
                         </a>
                     </div>
                     {/* Contact Info */}
